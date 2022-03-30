214872962c7ab194ca869564b8d6b0b1e04cd703
gerritt-luoma <gerritt.luoma@gmail.com>
2022-03-29 22:55:32 -0600

    Added search results

---
+++ jammming/src/components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SearchResults extends React.Component {
+  render() {
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <!-- Add a TrackList component -->
+    </div>
+  }
+}
+
+export default SearchResults;
