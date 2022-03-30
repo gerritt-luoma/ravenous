afff60396ad4a3a2b5bcc4297a1325cfe8881fbd
gerritt-luoma <gerritt.luoma@gmail.com>
2022-03-29 23:24:33 -0600

    Some cleanup to include and import all components

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import './Track.css';
+
 class Track extends React.Component {
   render() {
     return(
       <div className="Track">
         <div className="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3>filler</h3>
+          <p>artist | album</p>
         </div>
-        <button className="Track-action"><!-- + or - will go here --></button>
+        <button className="Track-action">{this.props.isRemoval ? '-': '+'}</button>
       </div>
     );
   }
