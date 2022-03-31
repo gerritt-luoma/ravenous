01fbfab531a7c293e89be0a9cc3dc1ea3dfa755a
gerritt-luoma <gerritt.luoma@gmail.com>
2022-03-31 14:35:38 -0600

    Added initial logic to change playlist name

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -34,6 +34,7 @@ class App extends React.Component{
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -52,6 +53,12 @@ class App extends React.Component{
     })
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name,
+    })
+  }
+
   render(){
     return (
       <div>
@@ -59,8 +66,16 @@ class App extends React.Component{
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <SearchResults 
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+            />
           </div>
         </div>
       </div>
