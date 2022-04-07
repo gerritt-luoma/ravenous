53117082cdc3e1ff44d1fb2f9afd6057d4d9e785
gerritt-luoma <gerritt.luoma@gmail.com>
2022-04-06 22:29:35 -0600

    Cleaned up Spotify object

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -1,38 +1,59 @@
+// Load client ID and retirect URI from .env.local
 const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const redirect_uri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+
+// Regex expressions for parsing href
 const accessTokenRE = /access_token=([^&]*)/;
 const expiresInRE = /expires_in=([^&]*)/;
-let accessToken;
 
+// Global access token
+let accessToken;
 
 const Spotify = {
   getAccessToken() {
+    // There are 3 cases to account for
+    // First, the access token has already been set for the session
     if(accessToken) {
       return accessToken
     }
     
+    // Parse accessToken from href
     const accessTokenREMatch = window.location.href.match(accessTokenRE);
     const expiresInREMatch = window.location.href.match(expiresInRE);
+    
+    // Second, the access token has just been passed to the href of the window
     if(accessTokenREMatch && expiresInREMatch) {
       accessToken = accessTokenREMatch[1];
       const expiresIn = Number(expiresInREMatch[1]);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
       return accessToken;
-    } else {
+    }
+    
+    // Third, there is no href so we must redirect to the authorization link
+    else {
       window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
     }
   },
 
   async search(term) {
+    // Always get the access token for each search to verify it is valid
     const access_token = Spotify.getAccessToken();
+
+    // Make API call
     let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
     });
+
+    // For now I have no error handling, always expecting a working case
+    // Error handling could be a good project in the future
     if(response.ok) {
       const responseJSON = await response.json();
+
+      // If the response has tracks return a list of objects
+      // else, return an empty list
       if(responseJSON.tracks) {
         return responseJSON.tracks.items.map(track => ({
           id: track.id,
@@ -45,7 +66,7 @@ const Spotify = {
     } else {
       return [];
     }
-  }
+  },
 }
 
 export default Spotify;
