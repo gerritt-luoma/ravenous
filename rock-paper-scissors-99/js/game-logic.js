87f0ef75504a21572c47c77c1f91d208f8f6e03a
gerritt-luoma <gerritt.luoma@gmail.com>
2022-04-12 21:49:21 -0600

    Started adding functionality to setPlayerMoves

--- rock-paper-scissors-99/js/game-logic.js
+++ rock-paper-scissors-99/js/game-logic.js
@@ -1,1 +1,44 @@
-// All code should be written in this file.
+// Globals for moves and strengths
+let playerOneMoveOneType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoType;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeType;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoType;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeType;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  if(player === 'Player One') {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if(player === 'Player Two') {
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
+}
+
+function getRoundWinner(roundNumber) {
+
+}
+
+function getGameWinner() {
+
+}
+
+function setComputerMoves() {
+
+}
