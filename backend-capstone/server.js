1a7852d697d39f5d72f6f7bfb528e987345e4b3e
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-18 14:25:20 -0700

    Added api router

---
+++ backend-capstone/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const errorhandler = require('errorhandler')
+const morgan = require('morgan');
+const apiRouter = require('./api/api');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+app.use(morgan('dev'));
+app.use('/api', apiRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
+
+
+module.exports = {
+    app
+}
