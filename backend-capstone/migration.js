823f10d434a97502f9acc154fce993a6bdb3ec9a
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-18 14:34:27 -0700

    Can create the tables

---
+++ backend-capstone/migration.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(() => {
+    // Remove table if it already exists
+    db.run("DROP TABLE IF EXISTS Artist", error => {
+        if(error) {
+            console.error(error);
+        }
+    });
+
+    // Create table
+    db.run("CREATE TABLE Artist (id INTEGER NOT NULL, name TEXT NOT NULL, date_of_birth TEXT NOT NULL, biography TEXT NOT NULL, is_currently_employed INTEGER DEFAULT 1, PRIMARY KEY('id'))", (error) => {
+        if(error) {
+            console.error(error);
+        }
+    })
+})
